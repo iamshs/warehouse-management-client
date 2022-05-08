6380e97bb5fd957a5b1d7e326cac910818473586
unknown <rdrifat91@gmail.com>
2022-05-09 02:31:13 +0600

    deleting data from ui

--- src/components/ManageInventories/ManageInventories.js
+++ src/components/ManageInventories/ManageInventories.js
@@ -4,19 +4,34 @@ import { Link } from "react-router-dom";
 import useInventories from "../../hooks/useInventories";
 
 const ManageInventories = () => {
-  const [inventories] = useInventories();
+  const [inventories,setInventories] = useInventories();
+  const handleDelete = id =>{
+    const proceed = window.confirm('Are you sure?');
+    if(proceed){
+        const url = `http://localhost:4000/inventory/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const remaining =inventories.filter(inventory => inventory._id !== id);
+            setInventories(remaining);
+        })
+    }
+}
   return (
     <div>
         <h3 className="text-center my-3">All Items</h3>
       {inventories.map((inventory) => (
-        <Table style={{backgroundColor: "rgb(218, 208, 182)"}} className='mx-auto w-100' striped  hover>
+        <Table key={inventory._id} style={{backgroundColor: "rgb(218, 208, 182)"}} className='mx-auto w-100' striped  hover>
         <thead >
           <tr >
             <th className="w-50">{inventory.name}</th>
             <th className="w-25">${inventory.price}</th>
             <th className="w-25">{inventory.quantity}</th>
             <th className="w-25">
-                <button className="btn btn-danger">DELETE</button>
+                <button className="btn btn-danger" onClick={()=>handleDelete(inventory._id)}>DELETE</button>
             </th>
           </tr>
         </thead>
